1d5acf7eb8c77783ae260d69d7548ae1e62c205a
EnioAmarantes <enio@forlogic.org>
2021-11-01 16:27:40 -0300

    Adicionado Interface para implementar os métodos da Base

---
+++ src/shared/IBase.java
@@ -0,0 +1,12 @@
+package shared;
+
+public interface IBase {
+	public void BackHome();
+	public void New();
+	public void Edit();
+	public void Cancel();
+	public void Clear();
+	public void Create();
+	public void Update();
+	public void Remove();
+}
